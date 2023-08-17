7af4487c77b02416dc9fd5f09f2fb43099d419c2
Youssefelharouch <elharouchyoussef1998@gmail.com>
2023-08-17 10:45:52 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Test function for positive or negative
- * Return: 0
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
- int i;
+    char c;
 
- i = 0;
- positive_or_negative(i);
-
-return (0);
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
 }
-
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,25 +1,18 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
- int i;
+    char c;
 
- printf("Infinite loop incoming :(\n");
-
- i = 0;
-
- /*while (i < 10)*/
- /*{*/
- /* putchar(i);*/
- /*}*/
-
- printf("Infinite loop avoided! \\o/\n");
-
- return (0);
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
 }
-
