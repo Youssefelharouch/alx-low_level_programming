607be54dd64cc45d655679728f72cee0a557b1b9
Youssefelharouch <elharouchyoussef1998@gmail.com>
2023-09-01 05:46:10 -0700

    first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: The number of arguments passed to the program (unused)
+ * @argv: An array of pointers to the command-line arguments
+ *
+ * Return: Always 0 (success)
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	/* Print the name of the program */
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,7 @@
+<h1 align="center">0x0A. C - argc, argv</h1>
+
+###
+
+<p align="left">How to use arguments passed to your program<br>What are two prototypes of main that you know of, and in which case do you use one or the other<br>How to use __attribute__((unused)) or (void) to compile functions with unused variables or parameters</p>
+
+###
