3efe8681d30f14403f0c8a1567b6c61323a91821
Youssef Elharouch <elharouchyoussef1998@gmail.com>
2023-12-04 11:55:35 +0100

    hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,22 +5,25 @@
 */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *tmp, *node;
 	hash_table_t *head = ht;
+	hash_node_t *tmp, *node;
 	unsigned long int i;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node != NULL)
+		if (ht->array[i] != NULL)
 		{
-			tmp = node->next;
-			free(node->key);
-			free(node->value);
-			free(node);
-			node = tmp;
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
